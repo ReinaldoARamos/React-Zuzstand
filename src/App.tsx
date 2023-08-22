dfdc725efa2c1ac824eb3564960b570b7986e6da
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-08-22 15:59:48 -0300

    configurando o tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Provider as ReduxProvider } from "react-redux";
 import { store } from "./store";
 import { Player } from "./pages/player";
+import  './styles/global.css'
 export default function App() {
   return (
     <div>
