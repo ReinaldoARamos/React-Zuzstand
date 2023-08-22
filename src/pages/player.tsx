c3b62c039cab5c01978de54085efd2ebfce6dc98
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-08-22 17:29:36 -0300

    como o tamanho do player do video é relativo ao do aside, se colocarmos muitos itemns ele fica aumentando, pra resolver isso foi colocado um scroll e um absolute no player, mas isso fazia o player preencher todo o container, pra isso colocamos um padding ridght 80, que é o tamanho do aside para deixar da maneira correta

--- src/pages/player.tsx
+++ src/pages/player.tsx
@@ -17,18 +17,102 @@ export function Player() {
             deixar feedback {<MessageCircle className="h-4 w-4" />}
           </button>
         </div>
-        <main className="relative flex overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 shadow">
+        <main className="relative flex overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 pr-80 shadow">
           <div className="flex-1  ">
-            <div className="w-full bg-zing-950 aspect-video">
-                <ReactPlayer 
+            <div className="bg-zing-950 aspect-video w-full">
+              <ReactPlayer
                 width="100%"
                 height="100%"
                 controls
                 url="https://www.youtube.com/watch?v=C0rJJceoO_0"
-                />
+              />
             </div>
           </div>
-          <aside className="bg-zing-900  w-80 border-l border-zinc-800">
+          <aside className="bg-zing-900  absolute bottom-0 right-0 top-0 w-80 overflow-y-scroll border-l border-zinc-800">
+            <div>
+              <button className="flex w-full items-center gap-3 bg-zinc-800 p-4 ">
+                <div className="flex h-10 w-10 items-center justify-center rounded-full bg-zinc-950 text-sm">
+                  1
+                </div>
+                <div className="flex flex-col gap-1 text-left">
+                  <strong>desvendando o redux</strong>
+                  <span className="text-xs text-zinc-400"></span>
+                </div>
+                <ChevronDown className="ml-auto h-6 w-6 text-zinc-400" />
+              </button>
+              <nav className="relative flex flex-col gap-4 p-6">
+                <button className="flex  items-center justify-center gap-3 text-sm text-zinc-400">
+                  <Video className="h-4 w-4 text-zinc-500" />
+                  <span>Fundamentod do redux</span>
+                  <span className="ml-auto font-mono text-xs text-zinc-500">
+                    09:13
+                  </span>
+                </button>
+                <button className="flex  items-center justify-center gap-3 text-sm text-zinc-400">
+                  <Video className="h-4 w-4 text-zinc-500" />
+                  <span>Fundamentod do redux</span>
+                  <span className="ml-auto font-mono text-xs text-zinc-500">
+                    09:13
+                  </span>
+                </button>
+                <button className="flex  items-center justify-center gap-3 text-sm text-zinc-400">
+                  <Video className="h-4 w-4 text-zinc-500" />
+                  <span>Fundamentod do redux</span>
+                  <span className="ml-auto font-mono text-xs text-zinc-500">
+                    09:13
+                  </span>
+                </button>
+                <button className="flex  items-center justify-center gap-3 text-sm text-zinc-400">
+                  <Video className="h-4 w-4 text-zinc-500" />
+                  <span>Fundamentod do redux</span>
+                  <span className="ml-auto font-mono text-xs text-zinc-500">
+                    09:13
+                  </span>
+                </button>
+              </nav>
+            </div>
+            <div>
+              <button className="flex w-full items-center gap-3 bg-zinc-800 p-4 ">
+                <div className="flex h-10 w-10 items-center justify-center rounded-full bg-zinc-950 text-sm">
+                  1
+                </div>
+                <div className="flex flex-col gap-1 text-left">
+                  <strong>desvendando o redux</strong>
+                  <span className="text-xs text-zinc-400"></span>
+                </div>
+                <ChevronDown className="ml-auto h-6 w-6 text-zinc-400" />
+              </button>
+              <nav className="relative flex flex-col gap-4 p-6">
+                <button className="flex  items-center justify-center gap-3 text-sm text-zinc-400">
+                  <Video className="h-4 w-4 text-zinc-500" />
+                  <span>Fundamentod do redux</span>
+                  <span className="ml-auto font-mono text-xs text-zinc-500">
+                    09:13
+                  </span>
+                </button>
+                <button className="flex  items-center justify-center gap-3 text-sm text-zinc-400">
+                  <Video className="h-4 w-4 text-zinc-500" />
+                  <span>Fundamentod do redux</span>
+                  <span className="ml-auto font-mono text-xs text-zinc-500">
+                    09:13
+                  </span>
+                </button>
+                <button className="flex  items-center justify-center gap-3 text-sm text-zinc-400">
+                  <Video className="h-4 w-4 text-zinc-500" />
+                  <span>Fundamentod do redux</span>
+                  <span className="ml-auto font-mono text-xs text-zinc-500">
+                    09:13
+                  </span>
+                </button>
+                <button className="flex  items-center justify-center gap-3 text-sm text-zinc-400">
+                  <Video className="h-4 w-4 text-zinc-500" />
+                  <span>Fundamentod do redux</span>
+                  <span className="ml-auto font-mono text-xs text-zinc-500">
+                    09:13
+                  </span>
+                </button>
+              </nav>
+            </div>
             <div>
               <button className="flex w-full items-center gap-3 bg-zinc-800 p-4 ">
                 <div className="flex h-10 w-10 items-center justify-center rounded-full bg-zinc-950 text-sm">
