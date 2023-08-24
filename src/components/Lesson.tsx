7919cc13f0b53b2b0c175a95e25f6b624a9b62d7
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-08-24 10:50:11 -0300

    criando a estilização da aula em destaque

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,20 +1,38 @@
-import { Video } from "lucide-react";
-
+import { PlayCircle, Video } from "lucide-react";
 
 interface LessonProps {
-  title: string,
-  minutesAmount: string,
-  onPlay: () => void
+  title: string;
+  minutesAmount: string;
+  isCurrent?: boolean;
+  onPlay: () => void;
 }
-export function Lesson({minutesAmount, title, onPlay}: LessonProps) {
 
-  
-
+export function Lesson({
+  minutesAmount,
+  title,
+  onPlay,
+  isCurrent = false,
+}: LessonProps) {
+  console.log(isCurrent);
   return (
-    <button className="flex  items-center justify-center gap-3 text-sm text-zinc-400" onClick={onPlay}>
-      <Video className="h-4 w-4 text-zinc-500" />
+    <button
+      className="flex  items-center justify-center gap-3 text-sm text-zinc-400 duration-150 data-[active=true]:text-emerald-400 enabled:hover:text-zinc-100"
+      onClick={onPlay}
+      disabled={isCurrent}
+      data-active={isCurrent}
+    >
+      {isCurrent ? (
+        <PlayCircle className="h-4 w-4 text-emerald-400 " />
+      ) : (
+        <Video className="h-4 w-4 text-zinc-500 " />
+      )}
       <span>{title}</span>
-      <span className="ml-auto font-mono text-xs text-zinc-500">{minutesAmount}</span>
+      <span
+        data-active={isCurrent}
+        className="ml-auto font-mono text-xs text-zinc-500  data-[active=true]:text-emerald-400"
+      >
+        {minutesAmount}
+      </span>
     </button>
   );
 }
--- src/components/Modules.tsx
+++ src/components/Modules.tsx
@@ -1,6 +1,6 @@
 import { ChevronDown } from "lucide-react";
 import { Lesson } from "./Lesson";
-import * as Collapsable from '@radix-ui/react-collapsible'
+import * as Collapsable from "@radix-ui/react-collapsible";
 import { useAppSelector } from "../store";
 import { useDispatch } from "react-redux";
 import { play } from "../store/slices/player";
@@ -11,11 +11,24 @@ interface ModuleProps {
 }
 
 export function Modules({ title, lessonsAmount, moduleIndex }: ModuleProps) {
-  const lessons = useAppSelector((state) => state.player.course.modules[moduleIndex].lessons)
-  const dispatch = useDispatch()
+  const lessons = useAppSelector(
+    (state) => {
+      return state.player.course.modules[moduleIndex].lessons
+    }
+  );
+
+  const dispatch = useDispatch();
+
+  const { currentLessonIndex, currentModuleIndex} = useAppSelector(state => {
+    const {currentLessonIndex, currentModuleIndex} = state.player
+
+    return {currentLessonIndex, currentModuleIndex}
+
+  })
+
+
   return (
     <Collapsable.Root className="group">
-
       <Collapsable.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4 ">
         <div className="flex h-10 w-10 items-center justify-center rounded-full bg-zinc-950 text-sm">
           {moduleIndex + 1}
@@ -24,14 +37,21 @@ export function Modules({ title, lessonsAmount, moduleIndex }: ModuleProps) {
           <strong>{title}</strong>
           <span className="text-xs text-zinc-400">{lessonsAmount} aulas</span>
         </div>
-        <ChevronDown className="ml-auto h-6 w-6 text-zinc-400 group-data-[state=open]:rotate-180 transition-transform duration-300" />
+        <ChevronDown className="ml-auto h-6 w-6 text-zinc-400 transition-transform duration-300 group-data-[state=open]:rotate-180" />
       </Collapsable.Trigger>
       <Collapsable.Content>
         <nav className="relative flex flex-col gap-4 p-6 transition duration-300">
           {lessons.map((lesson, lessonIndex) => {
+            const isCurrent = currentModuleIndex === moduleIndex && currentLessonIndex === lessonIndex;
+           
             return (
-              <Lesson minutesAmount={lesson.duration} title={lesson.title} onPlay={() => dispatch(play([moduleIndex, lessonIndex]))} />
-            )
+              <Lesson
+                minutesAmount={lesson.duration}
+                title={lesson.title}
+                onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
+                isCurrent={isCurrent}
+              />
+            );
           })}
         </nav>
       </Collapsable.Content>
